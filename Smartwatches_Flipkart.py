b8a2c4364bcb2f13172f7b856890c7fb685b6148
diya-santhosh29 <100279136+diya-santhosh29@users.noreply.github.com>
2023-02-08 18:59:28 +0530

    Updated

--- Smartwatches_Flipkart.py
+++ Smartwatches_Flipkart.py
@@ -1,3 +1,4 @@
+#Importing the necessary libraries
 import pandas as pd
 from selenium import webdriver
 import openpyxl
@@ -7,8 +8,9 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import  NoSuchElementException
 
 
-
+#Initiating empty lists
 smartwatch_name=[]
+smartwatch_brand=[]
 smartwatch_discounted_price=[]
 smartwatch_rating=[]
 smartwatch_original_price=[]
@@ -19,24 +21,28 @@ smartwatch_size=[]
 
 
 
-
-
 # List of top 10 smartwatch brands
-brand_name =["Samsung","boAt" , "Noise", "CrossBeats", "Fastrack", "ZEBRONICS", "TAGG", "Titan","FITBIT","APPLE"]
+brand_name =["Samsung","boAt","Noise", "CrossBeats", "Fastrack", "ZEBRONICS", "TAGG", "Titan","FITBIT","APPLE"]
+#Opening the driver
 driver = webdriver.Chrome('/home/diya/Downloads/chromedriver_linux64 (2)/chromedriver')
+driver.maximize_window()
+
+#Defining a function for appending fetched elements into a list
+def attribute_function(list_name,final_list_name):
+    for numbers in list_name:
+    final_list_name.append(numbers.text)
+
 
 # Giving the brand name and page numbers in a loop
 for brand in brand_name:
-    for page in range(1,5): #Scrape data of first 2 pages of each brand
+    for page in range(1,10): #Scrape data of first 9 pages of each brand
 
-
-        driver.maximize_window()
         # Navigate to the given URL
         driver.get("https://www.flipkart.com/search?q=smartwatches&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off&p%5B%5D=facets.brand%255B%255D%3D{}&page={}".format(brand, page))
-        #driver.implicitly_wait(20)
+        driver.implicitly_wait(10)
 
-
-        elems = driver.find_elements_by_class_name('_1fQZEK')
+        #Creating a list of product links
+        elems = driver.find_elements(By.CLASS_NAME,'_1fQZEK')
         product_link = []
         for elem in elems:
             href = elem.get_attribute('href')
@@ -44,31 +50,46 @@ for brand in brand_name:
             if href is not None:
                 print(href)
 
+
+        #Scraping the necessary attributes
         try:
+            #Parsing through different links from the list
             for links in product_link:
                 driver.get("{}".format(links))
-                product_name=driver.find_elements_by_xpath("//span[@class='B_NuCI']")
-                for name in product_name:
-                    smartwatch_name.append(name.text)
-                discounted_product_price = driver.find_elements_by_xpath("//div[@class='_30jeq3 _16Jk6d']")
-                for dis_price in discounted_product_price:
-                    smartwatch_discounted_price.append(dis_price.text)
-                product_rating = driver.find_elements_by_xpath("//div[@class='gUuXy- _16VRIQ']//child::div")
-                for rating in product_rating:
-                    smartwatch_rating.append(rating.text)
-                original_product_price = driver.find_elements_by_xpath("//div[@class='_3I9_wc _2p6lqe']")
-                for ori_price in original_product_price:
-                    smartwatch_original_price.append(ori_price.text)
+                #Appending brand name to the list
+                smartwatch_brand.append(brand)
+
+                #Scraping the product name
+                product_name=driver.find_elements(By.XPATH,"//span[@class='B_NuCI']")
+                attribute_function(product_name,smartwatch_name)
+
+                #Scraping discounted price
+                discounted_product_price = driver.find_elements(By.XPATH,"//div[@class='_30jeq3 _16Jk6d']")
+                attribute_function(discounted_product_price,smartwatch_discounted_price)
+
+                #Scraping product rating
+                product_rating = driver.find_elements(By.XPATH,"//div[@class='gUuXy- _16VRIQ']//child::div")
+                attribute_function(product_rating,smartwatch_rating)
+
+                #Scraping original price
+                original_product_price = driver.find_elements(By.XPATH,"//div[@class='_3I9_wc _2p6lqe']")
+                attribute_function(original_product_price,smartwatch_original_price)
+
+
+                #To get attributes from the specifications table
                 for specifications in range(1,15):
                     feature = driver.find_element(By.XPATH,("(//td[@class='_1hKmbr col col-3-12'])[position()={}]".format(specifications)))
                     try:
+                        #To get Strap Color
                         if (feature.text=="Strap Color"):
                             feature1= driver.find_element(By.XPATH,("(//td[@class='URwL2w col col-9-12'])[position()={}]".format(specifications)))
                             #print(feature1.text)
                             smartwatch_strap_color.append(feature1.text)
+                        #To get Model Number
                         elif feature.text == "Model Number":
                             feature2 = driver.find_element(By.XPATH,("(//td[@class='URwL2w col col-9-12'])[position()={}]".format(specifications)))
                             smartwatch_model.append(feature2.text)
+                        #To get Size
                         elif feature.text == "Size":
                             feature3= driver.find_element(By.XPATH,("(//td[@class='URwL2w col col-9-12'])[position()={}]".format(specifications)))
                             smartwatch_size.append(feature3.text)
@@ -77,8 +98,8 @@ for brand in brand_name:
         except NoSuchElementException:
             pass
 
-
-final=zip(smartwatch_name,smartwatch_discounted_price,smartwatch_original_price,smartwatch_rating,smartwatch_strap_color,smartwatch_model,smartwatch_size)
+#Merging the attributes
+final=zip(smartwatch_brand,smartwatch_name,smartwatch_discounted_price,smartwatch_original_price,smartwatch_rating,smartwatch_strap_color,smartwatch_model,smartwatch_size)
 
 # Initialize a new Excel workbook
 wb=openpyxl.Workbook()
@@ -86,12 +107,12 @@ wb=openpyxl.Workbook()
 sheet1=wb.active
 
 # Loop through the final list and add each row to the worksheet
-#sheet1.append=(['Product Name','Product Price', 'Product Rating'])
+#sheet1.append=(['Brand','Product Name','Discounted Price', 'Original Price', 'Product Rating','Strap Color','Model','Size'])
 for x in list(final):
     sheet1.append(x)
 
 # Save the workbook to the specified file name
-wb.save("SmartWatch_Flpikart_5.xlsx")
+wb.save("SmartWatch_Flpikart_6.xlsx")
 
 
 
